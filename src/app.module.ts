71034bfe91e1eb2940f1e69b60a32d31f3facd08
Dean Figueiredo <deangelly.figueiredo@gmail.com>
2024-03-28 23:19:22 -0300

    configuring postgresql container

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { UserModule } from './modules/user/infra/persistence/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5445,
-    username: 'postgres',
-    password: 'teste-dean',
-    database: 'user',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  })],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres-password',
+      database: 'user',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
